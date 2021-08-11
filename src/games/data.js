3632a1ddd41fe5a35b875d6cdb21835990121731
ans-human90 <68585853+ans-human90@users.noreply.github.com>
2021-08-11 17:09:58 +0530

    Inital setup

---
+++ src/games/data.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line
+export default {
+  ballObj: {
+    x: 20,
+    y: 200,
+    dx: 5,
+    dy: 5,
+    rad: 20,
+    speed: 5,
+  },
+  brickObj: {
+    x: 0.5,
+    y: 50,
+    width: 800 / 10 -1,
+    height: 20,
+    density: 2,
+    colors: ["red", "lightblue"],
+  },
+  player: {
+    name: "Anshuman",
+    lives: 5,
+    score: 0,
+    level: 1,
+  },
+  paddleProps: {
+    height: 20,
+    width: 100,
+    x: 100,
+    color: "orange",
+  },
+};
---
+++ src/games/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Board from './board'
+
+export default function Breakout() {
+    return (
+        <div>
+            <Board/>
+        </div>
+    )
+}
